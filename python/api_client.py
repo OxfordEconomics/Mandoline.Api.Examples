ab6fc98f4538ef07e3ad00bafbe63662d624c37e
JamesMillsOE <jamesmills@oxfordeconomics.com>
2017-03-14 09:26:38 -0400

    Added exception handling

--- python/api_client.py
+++ python/api_client.py
@@ -18,6 +18,7 @@ class Client(object):
         self._base_url = base_url
 
 
+    # returns: dictionary of headers for use with http requests
     def _header(self):
         headers = {'Accept':'application/json',
                    'API-Key': self.api_key,
@@ -26,7 +27,8 @@ class Client(object):
 
 
     # build api url using base url and path
-    # takes one or more strings and appends each to the base url
+    # takes: one or more strings and appends each to the base url
+    # returns: url string
     def _url(self, *path):
         url = self._base_url
         for q in path:
@@ -35,79 +37,146 @@ class Client(object):
 
 
     # get a full list of the databanks available as well as the number of countries for each
-    # returns: http response body, where json is a list of Databank objects 
+    # returns: list of json dictionaries of Databanks 
     def get_databanks(self):
         try:
             r = requests.get(self._url('databank'), headers=self._header())
             r.raise_for_status()
+
         except requests.exceptions.HTTPError as e:
             print(e)
             return {}
+
         except requests.exceptions.RequestException as e:
             print(e)
             return {}
+
         return r.json()
 
 
     # get a full list of a databank's available regions
     # takes: databank id
-    # returns: http response body, where json is a Region object
+    # returns: json dictionary of Regions 
     def get_databank_regions(self, id):
-        return requests.get(self._url('region', id), headers=self._header())
+        try:
+            r = requests.get(self._url('region', id), headers=self._header())
+
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            return {}
+
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return {}
+
+        return r.json()
 
 
-    # takes username, pass
-    # returns: if successful, http response body, where json is a User object
-    #          representing validated user
+    # takes: username, pass
+    # returns: json dictionary of validated User 
     def user_login(self, username, passwd):
         payload = { 'Username': username,
                     'Password': passwd }
-        return requests.post(self._url('users'), headers=self._header(), data=json.dumps(payload) )
+        try:
+            r = requests.post(self._url('users'), headers=self._header(), data=json.dumps(payload) )
+
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            return {}
+
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return {}
+
+        return r.json()
 
 
     # takes: selection id
-    # returns: http response body, where json is a Selection object
+    # returns: json dictionary of Selection 
     def get_selection(self, id):
-        return requests.get(self._url('savedselections', id), headers=self._header())
+        try:
+            r = requests.get(self._url('savedselections', id), headers=self._header())
+
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            return {}
+
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return {}
+
+        return r.json()
 
 
     # gets saved selection with given selection id (see above)
-    # returns: http response body, where json is the new Selection object
+    # returns: json dictinoary of new Selection
     def create_selection(self, selection_object):
-        return requests.post(self._url('savedselections'), headers=self._headers(), 
+        try:
+            r = requests.post(self._url('savedselections'), headers=self._headers(), 
                 data=json.dumps(selection_object) )
+
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            return {}
+
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return {}
+
+        return r.json()
 
 
     # updates a saved selection with given selection id (see above)
     # takes: selection object with id that corresponds to already existing non-temporary saved selection
-    # returns: http response body, where json is the updated Selection object
+    # returns: json dictionary of updated Selection
     def update_selection(self, selection_object):
-        return requests.put(self._url('savedselections', selection_object['Id']), headers=self._header(), 
-                data=json.dumps(selection_object) )
+        try:
+            r = requests.put(self._url('savedselections', selection_object['Id']), 
+                    headers=self._header(), data=json.dumps(selection_object) )
+
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            return {}
+
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return {}
+
+        return r.json()
 
 
-    # returns well-formed download url based on page and pagesize 
+    # returns: well-formed download url based on page and pagesize 
     def _download_url(self, page, pagesize):
         return self._url('download?includeMetadata=true&page={0}+&pagesize={1}').format(page,pagesize)
 
 
-    # download a selection from the macro databank
+    # returns: single page of downloaded data as json dictionary
     def _get_download(self, selection, page, pagesize=5):
-        return requests.post(self._download_url(page, pagesize), headers=self._header(), json=selection)
+        r = requests.post(self._download_url(page, pagesize), headers=self._header(), json=selection)
+        r.raise_for_status() # in case of bad request: pass http exceptions up to calling func
+        return r.json()
 
-   
-    # download a selection from the macro databank, broken into separate http requests with number 
-    # of entries in each download equal to pagesize
-    # returns: json dictionary 
+  
+    # downloads multiple pages of data based on provided Selection
+    # returns: json dictionary of Download data
     def get_download(self, selection, pagesize=5):
         page = 0
-        response = self._get_download(selection, page, pagesize)
-        new_data = response.json()
-        data = new_data
-        while len(new_data) == pagesize:
-            page += 1
-            response = self._get_download(selection, page, pagesize)
-            new_data = response.json()
-            data.append(new_data)
 
-        return data 
+        try:
+            new_data = self._get_download(selection, page, pagesize)
+            data = new_data
+            while len(new_data) == pagesize:
+                page += 1
+                response = self._get_download(selection, page, pagesize)
+                new_data = response.json()
+                data.append(new_data)
+
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            return {}
+
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return {}
+
+        return data
