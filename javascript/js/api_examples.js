daac9891e6e791f7644127dd74e30c681e27fba9
JamesMillsOE <jamesmills@oxfordeconomics.com>
2017-03-27 12:46:09 -0400

    JS: added saved selections

--- javascript/js/api_examples.js
+++ javascript/js/api_examples.js
@@ -4,6 +4,7 @@
 // root for full license information.
 // </copyright>
 
+const SELECTION_ID = '2c140fbb-4624-4004-927e-621734f3cb93'
 
 // sample selections
 var selectionA =
@@ -61,13 +62,49 @@ function postHttpResource(path, resource) {
 		success: function(data,status){
 			$("#log").empty();
 			$("#log").append(
-					+ "<pre><br/>" 
+					"<pre><br/>" 
 					+ JSON.stringify(data, null, 4) 
 					+ "</pre>"
 					);
 		},
 		error: function(data,status){
 		    alert(" Error Status: " + status);
+		    $("#log").empty();
+		    $("#log").append("Error running request");
+		}
+	});
+};
+
+// make post request to Mandoline API
+function putHttpResource(path, resource, resource_id="") {
+	if (resource_id)
+	{
+		resource_id = "/" + resource_id;
+	}
+
+	var hostname = $("#Hostname").val();
+	var api_url = hostname + "/api";
+	var api_key = $("#ApiKey").val();
+
+	var jsonResource = JSON.stringify(resource);
+	var apiHeader = { "Api-Key": api_key };
+	
+	$.support.cors = true;
+	
+	$.ajax({
+		url: encodeURI(api_url + path + resource_id),
+		type: 'PUT',
+		headers: apiHeader,
+		data: jsonResource,
+		contentType: 'application/json; charset=utf-8',
+		dataType: 'json',
+		success: function(data,status){
+			getHttpResource(path, resource_id)
+		},
+		error: function(data,status){
+		    alert(" Error Status: " + status);
+		    $("#log").empty();
+		    $("#log").append("Error running request");
 		}
 	});
 };
@@ -115,6 +152,8 @@ function getPagedData(resource, page) {
 		},
 		error: function(data,status){
 		    alert(" Error Status: " + status);
+		    $("#log").empty();
+		    $("#log").append("Error running request");
 		}
 	});
 };
@@ -149,12 +188,19 @@ function postLogin(path, resource) {
 		},
 		error: function(data,status){
 		    alert(" Error Status: " + status);
+		    $("#log").empty();
+		    $("#log").append("Error running request");
 		}
 	});
 };
 
 // make get request to Mandoline API
-function getHttpResource(path) {
+function getHttpResource(path, resource_id="") {
+	if (resource_id)
+	{
+		resource_id = "/" + resource_id;
+	}
+
 	var hostname = $("#Hostname").val();
 	var api_url = hostname + "/api";
 	var api_key = $("#ApiKey").val();
@@ -164,53 +210,130 @@ function getHttpResource(path) {
 	$.support.cors = true;
 	
 	$.ajax({
-		url: encodeURI(api_url + path),
+		url: encodeURI(api_url + path + resource_id),
 		type: 'GET',
 		headers: apiHeader,
 		contentType: 'application/json; charset=utf-8',
 		dataType: 'json',
-		success: function(data,status){
+		success: function(data, status){
 			$("#log").empty();
 			$("#log").append("<pre><br/>" + JSON.stringify(data, null, 4) + "</pre>");
 		},
 		error: function(data,status){
 		    alert(" Error Status: " + status);
+		    $("#log").empty();
+		    $("#log").append("Error running request");
 		}
 	});
 };
 
-
 function login() {
+    $("#log").empty();
+    $("#log").append("Loading...");
     var user_data = {
     	UserName: $("#user_name").val(),
     	Password: $("#user_pass").val()
     }
     postLogin('/users', user_data);
+    $("#user_pass").val("")
+};
+
+function getSelection() {
+    select_id = $("#selection_id").val();
+    $("#log").empty();
+    $("#log").append("Fetching selection: " + select_id + "...");
+    getHttpResource('/savedselections', select_id);
+};
+
+function createSelection() {
+    var selection_object = selectionA;
+    var dt = new Date();
+    selection_object.Name ="Selection - (Updated: " + dt.getFullYear() + '/'
+	+ (dt.getMonth() + 1) + "/"
+	+ dt.getDate() + " at "
+	+ dt.getHours() + ":" + dt.getMinutes() + ")";
+    $("#log").empty();
+    $("#log").append("Creating selection...");
+    postHttpResource('/savedselections', selection_object);
+};
+
+function updateSelection() {
+	$("#log").empty();
+	$("#log").append("Loading...");
+	var hostname = $("#Hostname").val();
+	var select_id = $("#selection_id").val()
+	var url = hostname + "/api/savedselections/" + select_id;
+	var api_key = $("#ApiKey").val();
+
+	var apiHeader = { "Api-Key": api_key };
+	
+	$.support.cors = true;
+	
+	$.ajax({
+		url: encodeURI(url),
+		type: 'GET',
+		headers: apiHeader,
+		contentType: 'application/json; charset=utf-8',
+		dataType: 'json',
+		success: function(data, status){
+		    var dt = new Date();
+		    data.Name ="Selection - (Updated: " + dt.getFullYear() + '/'
+			+ (dt.getMonth() + 1) + "/"
+			+ dt.getDate() + " at "
+			+ dt.getHours() + ":" + dt.getMinutes() + ")";
+		    putHttpResource('/savedselections', data, data['Id']);
+		},
+		error: function(data,status){
+		    alert(" Error Status: " + status);
+		    $("#log").empty();
+		    $("#log").append("Error running request");
+		}
+	});
 };
 
 function getUser() {
+    $("#log").empty();
+    $("#log").append("Loading...");
     getHttpResource('/users/me');
 };
 
 function getDatabanks() {
+    $("#log").empty();
+    $("#log").append("Loading...");
     getHttpResource('/databank');
 };
 
 function getVariables() {
+    $("#log").empty();
+    $("#log").append("Loading...");
     getHttpResource('/variable/WDMacro');
 };
 
 function getRegions() {
+    $("#log").empty();
+    $("#log").append("Loading...");
     getHttpResource('/region/WDMacro');
 };
 
 function getData(selection) {
+    $("#log").empty();
+    $("#log").append("Loading...");
     postHttpResource('/download?includeMetadata=true', selection);
 };
 
 
-// event handlers for the various buttons on our page
+// add event handlers to the various buttons on our page
 $(document).ready(function(){
+    $("#selection_id").val(SELECTION_ID);
+    $("#btnCreateSelection").click(function() {
+	    createSelection()
+    });
+    $("#btnUpdateSelection").click(function() {
+	    updateSelection()
+    });
+    $("#btnGetSelection").click(function() {
+	    getSelection()
+    });
     $("#btnLogin").click(function() {
     	login()
     });
